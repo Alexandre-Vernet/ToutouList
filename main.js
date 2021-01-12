2eb80073a661d2d85a7d7f08781fea0313cfe6b2
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2021-01-12 12:00:53 +0100

    Add list of object

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
+// List of tasks
+const task = {
+    task1: {
+        name: "Tâche 1",
+        state: 0
+    },
+    task2: {
+        name: "Tâche 2",
+        state: 0
+    },
+    task3: {
+        name: "Tâche 3",
+        state: 0
+    },
+    task4: {
+        name: "Tâche 4",
+        state: 0
+    }
+}
+
+// Display list of tasks
+function loadTasks() {
+
+    // Display list of tasks
+    for (let i = 0; i < Object.keys(task).length; i++)
+    {
+        // Display html code here :/
+        console.log(task.task1.name);
+    }
+}
+
+// Add task to object
 function addTask() {
     var taskToAdd = document.getElementById("taskToAdd").value;
 
@@ -6,9 +38,11 @@ function addTask() {
         alert('Textbox cannot be empty !');
     else
     {
-        alert('Adding task ...');
+        // Add task to Object here
     }
 }
+
+// Cancel task
 function cancelTask() {
     document.getElementById("taskToAdd").value = "";
 }
@@ -19,11 +53,11 @@ function KeyCheck(event) {
     var KeyID = event.keyCode;
     switch (KeyID)
     {
-        // Entrée
+        // Enter
         case 13:
             addTask();
             break;
-        // Echap
+        // Escape
         case 27:
             cancelTask();
             break;
